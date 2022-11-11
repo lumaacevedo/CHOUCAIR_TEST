c11376214e5715f2e9e825347b7fca2e3a441759
CaesarAl24 <57425795+lumaacevedo@users.noreply.github.com>
2022-11-10 23:26:33 -0500

    Proyecto de seleccion ajuste

--- src/test/java/co/com/choucair/certification/proyectoseleccion/runners/RunnerTags.java
+++ src/test/java/co/com/choucair/certification/proyectoseleccion/runners/RunnerTags.java
@@ -11,4 +11,5 @@ import org.junit.runner.RunWith;
                               glue = "co.com.choucair.certification.proyectoseleccion.stepdefinitions",
                               snippets = SnippetType.CAMELCASE)
 public class RunnerTags {
+
 }
