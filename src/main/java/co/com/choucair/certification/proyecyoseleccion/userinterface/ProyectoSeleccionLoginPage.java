532ec83864cca5b526da58eb1b03776b5af7a0be
CaesarAl24 <57425795+lumaacevedo@users.noreply.github.com>
2022-11-15 22:23:25 -0500

    Ajustes faltantes a las acciones tercera pagina

--- src/main/java/co/com/choucair/certification/proyecyoseleccion/userinterface/ProyectoSeleccionLoginPage.java
+++ src/main/java/co/com/choucair/certification/proyecyoseleccion/userinterface/ProyectoSeleccionLoginPage.java
@@ -8,4 +8,5 @@ import org.openqa.selenium.By;
 public class ProyectoSeleccionLoginPage extends PageObject {
     public static final Target LOGIN_BUTTON = Target.the("button that shows us the form to Register")
             .located(By.xpath("/html/body/ui-view/unauthenticated-container/div/div/unauthenticated-header/div/div[3]/ul[2]/li[2]/a"));
+
 }
